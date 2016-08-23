0263630ec7ad90e2d2d2bbdf5051cf39e095863c
Pedro Piñera Buendía <pepibumur@gmail.com>
2016-08-23 18:07:26 +0200

    Start with OAuth2WKNavigationDelegate tests

---
+++ Example/Tests/Platforms/iOS/OAuth2WKNavigationDelegateSpec.swift
@@ -0,0 +1,9 @@
+//
+//  OAuth2WKNavigationDelegateSpec.swift
+//  Paparajote
+//
+//  Created by Pedro Pinera Buendia on 23/08/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
